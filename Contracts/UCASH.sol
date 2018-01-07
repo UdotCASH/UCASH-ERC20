1b64bb4b53f6e8978969d32a8543dd28dcd8ee8f
Antoine De Vuyst <antoinedevuyst@Antoines-MacBook-Pro.local>
2018-01-07 08:54:42 -0500

    Contracts/UCASH.sol

--- Contracts/UCASH.sol
+++ Contracts/UCASH.sol
@@ -1,34 +1,59 @@
 /*
-This is the UCASH contract
-It the EIP20 token standard: https://github.com/ethereum/EIPs/issues/20
+This is the UCASH Ethereum smart contract
 
+UCASH Implements the EIP20 token standard: https://github.com/ethereum/EIPs/issues/20
 
-Visit https://u.cash
+The smart contract code can be viewed here: https://github.com/UdotCASH/UCASH-ERC20.git
+
+For more info about UCASH and the U.CASH ecosystem, visit https://u.cash
 .*/
+
 pragma solidity ^0.4.8;
 
-
 contract EIP20Interface {
-
-    /// total amount of UCASH
+    /* This is a slight change to the ERC20 base standard.
+    function totalSupply() constant returns (uint256 supply);
+    is replaced with:
+    uint256 public totalSupply;
+    This automatically creates a getter function for the totalSupply.
+    This is moved to the base contract since public getter functions are not
+    currently recognised as an implementation of the matching abstract
+    function by the compiler.
+    */
+    /// total amount of tokens
     uint256 public totalSupply;
 
-    /// returns the UCASH balance
+    /// @param _owner The address from which the balance will be retrieved
+    /// @return The balance
     function balanceOf(address _owner) public view returns (uint256 balance);
 
-    /// transfer UCASH from your address
+    /// @notice send `_value` token to `_to` from `msg.sender`
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
     function transfer(address _to, uint256 _value) public returns (bool success);
 
+    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+    /// @param _from The address of the sender
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
 
+    /// @notice `msg.sender` approves `_spender` to spend `_value` tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @param _value The amount of tokens to be approved for transfer
+    /// @return Whether the approval was successful or not
     function approve(address _spender, uint256 _value) public returns (bool success);
 
+    /// @param _owner The address of the account owning tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @return Amount of remaining tokens allowed to spent
     function allowance(address _owner, address _spender) public view returns (uint256 remaining);
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
-
 
 
 contract UCASH is EIP20Interface {
@@ -43,15 +68,14 @@ contract UCASH is EIP20Interface {
      function UCASH(
 
         ) public {
-        totalSupply = 21*10**9*10**8;                        // Set UCASH totalSupply
-        balances[msg.sender] = totalSupply;               // Give the creator all initial tokens
-        name = "UCASH";                                   // Set the name for display purposes
-        decimals = 8;                            // Amount of decimals for display purposes
-        symbol = "UCASH";                               // Set the symbol for display purposes
+        totalSupply = 21*10**9*10**8;               //UCASH totalSupply
+        balances[msg.sender] = totalSupply;         //Allocate UCASH to contract deployer
+        name = "UCASH";
+        decimals = 8;                               //Amount of decimals for display purposes
+        symbol = "UCASH";
     }
 
     function transfer(address _to, uint256 _value) public returns (bool success) {
-
         require(balances[msg.sender] >= _value);
         balances[msg.sender] -= _value;
         balances[_to] += _value;
@@ -60,7 +84,6 @@ contract UCASH is EIP20Interface {
     }
 
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-
         uint256 allowance = allowed[_from][msg.sender];
         require(balances[_from] >= _value && allowance >= _value);
         balances[_to] += _value;
